54d9b093654b1afbd51399a9a1eaee4eac85ee15
cmmorganX <146145038+cmmorganX@users.noreply.github.com>
2024-02-12 23:05:45 -0600

    Delete C++/Math1 directory

--- C++/Math1/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    int x = 10;   // integer X = 10
-    int y = x++;  // integer y = 10 , integer x = 11
-    int z = ++x;  // integer z = 11 , integer x = 11
-    std::cout << x;
-    return 0;
-}
