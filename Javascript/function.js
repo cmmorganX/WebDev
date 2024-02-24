483b90a3254ae693d963ae58ab0353ca0902cdbf
cmmorganX <146145038+cmmorganX@users.noreply.github.com>
2024-02-23 21:07:48 -0600

    Add files via upload

---
+++ Javascript/function.js
@@ -0,0 +1,12 @@
+//performing a task
+function greet(name, lastName) {
+
+    console.log('Hello ' + name + ' ' + lastName);
+}
+//function declarations do not need a semicolon at the end
+
+
+greet('Sally', 'Smith');
+greet('Mark', 'Mathers')
+//here we add semicolon to declare the statement
+
